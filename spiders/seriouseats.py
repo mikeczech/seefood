e157f97cd4d0dacd8f43e9aac86953e1e70484b3
Mike Czech <mail@mczech.de>
2019-05-31 10:21:42 +0200

    Scrape ingredients and directions

--- spiders/seriouseats.py
+++ spiders/seriouseats.py
@@ -32,7 +32,9 @@ class SeriousEatsSpider(scrapy.Spider):
         for module in response.css("section#recipes div.module"):
             title = module.css("div.metadata a h4.title").xpath("string(.)").get()
             if title:
-                short_description = module.css("div.metadata a p.kicker").xpath("string(.)").get()
+                short_description = (
+                    module.css("div.metadata a p.kicker").xpath("string(.)").get()
+                )
                 thumbnail = module.css("img").xpath("@data-src").get()
                 url = module.css("a").xpath("@href").get()
                 request = response.follow(url, self.parse_recipe_page)
@@ -43,10 +45,24 @@ class SeriousEatsSpider(scrapy.Spider):
                     "url": url,
                 }
                 yield request
-        
 
     def parse_recipe_page(self, response):
-        description = response.css("div.recipe-introduction div.recipe-introduction-body").xpath("string(p[2])").get()
+        description = (
+            response.css("div.recipe-introduction div.recipe-introduction-body")
+            .xpath("string(p[2])")
+            .get()
+        )
+        directions = "||||".join(
+            filter(
+                lambda x: x != "",
+                response.css("div.recipe-procedure-text p").xpath("string(.)").getall(),
+            )
+        )
+        ingredients = "||||".join(
+            response.css("li.ingredient").xpath("string(.)").getall()
+        )
         data = response.meta["data"]
         data["description"] = description
+        data["directions"] = directions
+        data["ingredients"] = ingredients
         yield data
