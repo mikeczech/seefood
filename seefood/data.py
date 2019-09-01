880c31f2ddbfaf60d3eb4cf9b0b3b2ecb109205d
Mike Czech <mail@mczech.de>
2019-09-01 12:57:30 +0200

    Implement label generation as scikit learn transformers

--- seefood/data.py
+++ seefood/data.py
@@ -1,5 +1,6 @@
 import re
 import os
+import itertools
 
 import torch
 from torchvision import transforms
@@ -7,45 +8,43 @@ from torchvision import transforms
 from PIL import Image
 
 import gensim
+from gensim import corpora
 import spacy
+import nltk
 from nltk.corpus import stopwords
 
 from sklearn.pipeline import Pipeline
 from sklearn.base import BaseEstimator, TransformerMixin
 
 
-class ImageRead(BaseEstimator, TransformerMixin):
-    def __init__(self, image_base_path, image_size):
-        self.image_base_path = image_base_path
-        self.transform_img = transforms.Compose(
-            [
-                transforms.Resize(image_size),
-                transforms.CenterCrop(image_size),
-                transforms.ToTensor(),
-                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
-            ]
-        )
+class ThumbnailUrlTransformer(BaseEstimator, TransformerMixin):
+    def __init__(self, src_col, target_col, image_base_path):
+        self._src_col = src_col
+        self._target_col = target_col
+        self._image_base_path = image_base_path
 
     def fit(self, X, y=None):
-        pass
+        return self
 
-    def transform(self, X, y=None):
-        X["thumbnail"] = X["thumbnail"].apply(
-            lambda x: os.path.join(self.image_base_path, x.rsplit("/", 1)[-1])
+    def transform(self, X):
+        X = X.assign(
+            **{
+                self._target_col: X[self._src_col].apply(
+                    lambda x: os.path.join(self._image_base_path, x.rsplit("/", 1)[-1])
+                )
+            }
         )
-        X = X[X["thumbnail"].apply(lambda x: os.path.isfile(x))]
 
-        X["thumbnail"] = X["thumbnail"].apply(
-            lambda img_path: Image.open(img_path).convert("RGB")
-        )
-        X["thumbnail"] = X["thumbnail"].apply(lambda img: self.transform_img(img))
+        X = X[X[self._target_col].apply(lambda x: os.path.isfile(x))]
 
         return X
 
 
-class PreprocessText(BaseEstimator, TransformerMixin):
-    def __init__(self, col_name):
-        self._col_name = col_name
+class BasicTextTransformer(BaseEstimator, TransformerMixin):
+    def __init__(self, src_col, target_col):
+        self._src_col = src_col
+        self._target_col = target_col
+        nltk.download("stopwords")
         self._nlp = spacy.load("en", disable=["parser", "ner"])
         self._stop_words = stopwords.words("english")
         self._stop_words.extend(
@@ -88,19 +87,21 @@ class PreprocessText(BaseEstimator, TransformerMixin):
     def make_trigrams(self, sents, bigram_mod, trigram_mod):
         return [trigram_mod[bigram_mod[s]] for s in sents]
 
-    def lemmatization(self, sents):
+    def lemmatization(self, sents, allowed_postags=["NOUN"]):
         sents_out = []
         for s in sents:
             doc = self._nlp(" ".join(s))
-            sents_out.append([token.lemma_ for token in doc])
+            sents_out.append(
+                [token.lemma_ for token in doc if token.pos_ in allowed_postags]
+            )
         return sents_out
 
     def fit(self, X, y=None):
-        pass
+        return self
 
-    def transform(self, X, y=None):
-        sents = X[self._col_name].apply(
-            lambda x: gensim.utils.simple_preprocess(x, deacc=True)
+    def transform(self, X):
+        sents = X[self._src_col].apply(
+            lambda x: gensim.utils.simple_preprocess(str(x), deacc=True)
         )
         bigram = gensim.models.Phrases(sents, min_count=10, threshold=100)
         trigram = gensim.models.Phrases(bigram[sents], threshold=100)
@@ -109,17 +110,21 @@ class PreprocessText(BaseEstimator, TransformerMixin):
 
         nostops = self.remove_stopwords(sents)
         bigrams = self.make_bigrams(nostops, bigram_mod)
-        trigrams = self.make_trigrams(
-            bigrams, bigram_mod, trigram_mod
-        )
-        X[self._col_name] = self.lemmatization(trigrams)
+        trigrams = self.make_trigrams(bigrams, bigram_mod, trigram_mod)
+
+        X = X.assign(**{self._target_col: self.lemmatization(trigrams)})
 
         return X
 
 
-class TitleTransform(BaseEstimator, TransformerMixin):
-    def __init__(self):
-        pass
+class UnclutterTitleTansformer(BaseEstimator, TransformerMixin):
+    """
+    Removes meaningless parts of titles (e.g. 'The best ...').
+    """
+
+    def __init__(self, src_col, target_col):
+        self._src_col = src_col
+        self._target_col = target_col
 
     @staticmethod
     def delete_preamble(title):
@@ -143,14 +148,76 @@ class TitleTransform(BaseEstimator, TransformerMixin):
         return title
 
     def fit(self, X, y=None):
-        pass
+        return self
 
     def transform(self, X, y=None):
-        X["title"] = X["title"].map(TitleTransform.delete_preamble)
-        X["title"] = X["title"].map(TitleTransform.get_title_post_description)
-        X["title"] = X["title"].map(TitleTransform.remove_the_best_prefix)
+        X = X.assign(
+            **{
+                self._target_col: X[self._src_col].map(
+                    UnclutterTitleTansformer.delete_preamble
+                )
+            }
+        )
+        X[self._target_col] = X[self._target_col].map(
+            UnclutterTitleTansformer.get_title_post_description
+        )
+        X[self._target_col] = X[self._target_col].map(
+            UnclutterTitleTansformer.remove_the_best_prefix
+        )
 
         return X
+
+
+class CreateLabelsTransformer(BaseEstimator, TransformerMixin):
+    """
+    Creates image labels from preprocessed titles and descriptions
+    """
+
+    def __init__(
+        self, title_col, description_col, target_col, title_word_count_threshold=60
+    ):
+        self._title_col = title_col
+        self._description_col = description_col
+        self._target_col = target_col
+        self._title_word_count_threshold = title_word_count_threshold
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X, y=None):
+        # unclutter descriptions
+        title_dict = corpora.Dictionary(X[self._title_col])
+        filtered_title_vocab = [
+            token
+            for token in set(itertools.chain(*X[self._title_col]))
+            if title_dict.dfs[title_dict.token2id[token]]
+            > self._title_word_count_threshold
+        ]
+        uncluttered_descriptions = X[self._description_col].map(
+            lambda d: [t for t in d if t in filtered_title_vocab]
+        )
+
+        # build labels
+        labels = [t + d for t, d in zip(X[self._title_col], uncluttered_descriptions)]
+        labels_dict = corpora.Dictionary(labels)
+        high_freq_labels = [
+            [t for t in l if labels_dict.dfs[labels_dict.token2id[t]] > 50]
+            for l in labels
+        ]
+        X = X.assign(**{self._target_col: [set(sent) for sent in high_freq_labels]})
+
+        return X
+
+
+class SelectColumnsTransformer(BaseEstimator, TransformerMixin):
+    def __init__(self, columns):
+        self._columns = columns
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X, y=None):
+        return X[self._columns]
 
 
 class RecipeDataset(torch.utils.data.Dataset):
