4718afaf5697a2eadee067e167ba3f1c32d94d1a
Mike Czech <mike.czech@ottogroup.com>
2019-12-29 13:31:33 +0100

    Add main menu in iOS app

--- ios/Seefood/Home.swift
+++ ios/Seefood/Home.swift
@@ -10,7 +10,51 @@ import SwiftUI
 
 struct Home: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Spacer()
+            HStack(alignment: .top) {
+                VStack {
+                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                        Image(systemName: "house.fill")
+                            .imageScale(.large)
+                            .foregroundColor(.white)
+                    }
+                    Text("Home".uppercased())
+                        .font(.caption)
+                        .padding(5)
+                        .foregroundColor(.white)
+                }
+                .padding()
+                .frame(width: 80.0, height: 80.0)
+                Spacer()
+                VStack {
+                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                        ZStack {
+                            Circle()
+                                .frame(width: 85.0, height: 85.0)
+                                .foregroundColor(.pink)
+                            Image(systemName: "plus.circle.fill")
+                                .resizable()
+                                .frame(width: 70.0, height: 70.0)
+                                .foregroundColor(.white)
+                        }
+                    }.offset(x:0, y:-20)
+                }
+                Spacer()
+                VStack {
+                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                        Image(systemName: "person.circle.fill")
+                            .imageScale(.large)
+                            .foregroundColor(.white)
+                    }
+                    Text("me".uppercased())
+                        .font(.caption)
+                        .padding(5)
+                        .foregroundColor(.white)
+                }.padding()
+                .frame(width: 80.0, height: 80.0)
+            }.background(Color.pink)
+        }.edgesIgnoringSafeArea(.bottom)
     }
 }
 
