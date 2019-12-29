6ed51ae177738fc698d0b472f7b0fc26616a1d4e
Mike Czech <mike.czech@ottogroup.com>
2019-12-29 11:02:39 +0100

    Add skeleton for iOS app

---
+++ ios/Seefood/Home.swift
@@ -0,0 +1,21 @@
+//
+//  Home.swift
+//  Seefood
+//
+//  Created by Mike Czech on 29.12.19.
+//  Copyright © 2019 Mike Czech. All rights reserved.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Home_Previews: PreviewProvider {
+    static var previews: some View {
+        Home()
+    }
+}
---
+++ ios/SeefoodTests/SeefoodTests.swift
@@ -0,0 +1,34 @@
+//
+//  SeefoodTests.swift
+//  SeefoodTests
+//
+//  Created by Mike Czech on 29.12.19.
+//  Copyright © 2019 Mike Czech. All rights reserved.
+//
+
+import XCTest
+@testable import Seefood
+
+class SeefoodTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
